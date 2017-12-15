21f12e62cfcec2f738f0bc7eef44388f970c2f9d
Jeff Buttars <jeff@jeffbuttars.com>
2017-12-15 11:44:44 -0800

    Initial

---
+++ RoboCode/lesson1/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        int teamID = 5871;
+        String myName = "Jeff";
+
+        System.out.println(teamID);
+        System.out.println(myName);
+        System.out.println(myName + " is on Team " + teamID + "!!!");
+    }
+}
